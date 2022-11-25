fa5240289c4684b55e8862ab0c0eb9426664d2be
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2022-11-25 17:35:06 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,15 @@
+function solution(dot) {
+    if(dot[0]*dot[1]>0){
+        if(dot[0]>0)
+            return 1;
+        else
+            return 3;
+        
+    }else{
+        if(dot[0]>0)
+            return 4;
+        else
+            return 2;
+        
+    }
+}
