b3bdd6a8ea880763fd1c91415e2b41a66ecea45a
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2022-12-03 17:29:16 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    if(x*y>0){
+        if(x>0)printf("1");
+        else printf("3");
+    }else{
+        if(x<0)printf("2");
+        else printf("4");
+    }
+
+}
