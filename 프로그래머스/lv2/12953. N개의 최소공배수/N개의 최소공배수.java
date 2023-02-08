81c95824d1ce770c6dbecabad1dbfcd0c5cf2fa6
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-02-08 15:42:35 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        
+        if(arr.length == 1) return arr[0];
+        int gcd = gcd(arr[0], arr[1]);
+         
+        answer = (arr[0]*arr[1])/gcd;
+    
+        if(arr.length>2){
+             for(int i = 2; i < arr.length; i++) {
+                gcd = gcd(answer, arr[i]);
+                answer = (answer * arr[i]) / gcd;
+            }
+     
+        
+        }
+         
+        
+        return answer;
+    }
+    
+    public int gcd(int a,int b){
+      int r = a % b;
+        if(r == 0) return b;
+        else return gcd(b, r);
+    }
+    
+}
