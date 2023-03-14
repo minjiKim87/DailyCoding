bb67cc866c83af9cbb84d0fd40d9583a94ca6a16
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-03-14 22:13:24 +0900

    [level 2] Title: [1차] 캐시, Time: 2.96 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        
+        if(cacheSize == 0)
+            return cities.length*5;
+        
+        LinkedList<String> cache = new LinkedList<>();
+        for(int i = 0; i<cities.length;i++){
+            String s = cities[i].toUpperCase();
+            if(cache.remove(s)){
+                answer+=1;
+                cache.add(s);
+            }else{
+                answer += 5;
+                if(cache.size() >= cacheSize){
+                    cache.remove(0);
+                }
+                cache.add(s);
+            }
+        }
+        return answer;
+        
+    
+    }
+}
