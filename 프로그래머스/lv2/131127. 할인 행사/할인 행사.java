85b76eff3a747796cd215e8c919204bdf7f5ec3c
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-07-07 16:16:31 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+   
+        ArrayList <String> list = new ArrayList<>();
+
+        int wantNum = want.length;
+        int num=0;
+        for(int i =0;i<number.length;i++){
+           num+= number[i];
+        }
+        int check =0;
+        for(int i=0;i<discount.length-num+1;i++){//시작점 
+            check=0;
+            list.clear();
+            for(int j=i;j<i+num;j++){
+                list.add(discount[j]);
+                //System.out.print(discount[j]+" ");
+            }
+         //  System.out.println();
+           
+            for(int j =0;j<wantNum;j++){
+             //   System.out.print(want[j]+": "+number[j] +"? "+(Collections.frequency(list, want[j])+" "));
+                if(Collections.frequency(list, want[j])==number[j]){
+                            
+                }                  
+                else{  
+                    check++;
+                    break;
+                }                
+                  
+            }
+            if(check==0)
+                    answer++;
+            
+        }
+    
+    
+         return answer;
+        
+}
+}
