b6e884f7a303c3c4f606cf8b2c2f472c7bfa38a7
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-09-01 16:47:05 +0900

    [unrated] Title: 우박수열 정적분, Time: 40.94 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,53 +1,47 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<int[]> list = new ArrayList<>();
     
     public double[] solution(int k, int[][] ranges) {
 
-        list.add(k);
+        list.add(new int[]{0, k});
         collatz(k);
         int n = list.size();
-               
-        double[] answer = new double[ranges.length];
-             
-        
-        int[] arr = new int[n+1];
-        for(int i =0;i<n;i++){
+        int[][] arr = new int[n][];
+        for(int i =0;i<list.size();i++){
             arr[i] = list.get(i);
-            //System.out.print(arr[i]+" ");
+            System.out.print(arr[i][1]+" ");
         }
-       // System.out.println();
+        System.out.println();
         
-        double[] trape = new double[n];
+        double[] trape = new double[arr.length];
         
-        for(int i=0;i<n;i++){
-              trape[i] = Math.abs((double)(arr[i]+arr[i+1])/2);
-            //System.out.println(trape[i]);
+        for(int i=0;i<n-1;i++){
+              trape[i] = Math.abs((double)(arr[i][1]+arr[i+1][1])/2);
+            System.out.println(trape[i]);
         }
         
-         double[] prefixSum = new double[n];
-        prefixSum[0] = trape[0];
-        for(int i=0; i<n-1; i++){
-            prefixSum[i+1] = (trape[i] + prefixSum[i]);
-            //System.out.println(prefixSum[i+1]);
-        }
- 
-        
-         for(int i=0; i<ranges.length; i++){
-            int s = ranges[i][0];
-            int e = n-1 + ranges[i][1];
-
-            if(e > s){
-                double val = prefixSum[e] - prefixSum[s];
+        double[] answer = new double[n];
+        for(int i =0;i<ranges.length;i++){
+            int a = ranges[i][0];
+            int b = ranges[i][1];
+            if(b<=0)
+                b = n - ranges[i][1];
+            
+            if(a<b)
+                answer[i] = -1.0;
+                
+            if(a>b){
+                double val = trape[a]-trape[b];
                 String str = String.format("%.1f", val);
                 answer[i] = (Double.parseDouble(str));
-            }else if(s > e){
-                answer[i] = -1.0;
-            }else{
-                answer[i] = 0.0;
             }
+            
         }
+        
+        
+        
         
         return answer;
     }
@@ -61,7 +55,7 @@ class Solution {
             else
                 n=3*n+1;
             
-            list.add(n);
+            list.add(new int[]{list.size(), n});
             if(n==1)
                 break;
         }
