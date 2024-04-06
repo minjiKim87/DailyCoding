f72929eac6c2fd004648abded8e9f203f7ef0e09
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2024-04-06 12:58:18 +0900

    [level 1] Title: 폰켓몬, Time: 3.04 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -6,17 +6,25 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int N = nums.length/2;
         
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n:nums){
-            map.put(n, map.getOrDefault(n, 0)+1);
-        }
+        /*
+        포켓몬 nums.length()만큼 있다
+        선택 개수 N/2마리
+        같은 종류 = 같은 번호 : [3,1,2,3] -> [1,2,3,3] 정렬?
+        최대한 많은 종류의 포켓몬을 선택하는 
+        경우의 수X
+        그냥 그 방법만 찾아서 종류 몇개인지 리턴
+        중복제거?
+        */
         
-        if(map.size()>=N){
-            answer = N;
+        int get = nums.length;
+        LinkedHashSet<Integer> linkedHashSet =
+            new LinkedHashSet<>(Arrays.stream(nums) .boxed()
+                        .collect(Collectors.toList()));
+        if(linkedHashSet.size()>get/2){
+            answer = get/2;
         }else{
-            answer = map.size();
+            answer = linkedHashSet.size();
         }
         
         return answer;
