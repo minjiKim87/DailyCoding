8d1d531154d375e26d1721072365a7e6b5b34e3b
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2024-04-08 11:32:13 +0900

    [level 2] Title: 모음 사전, Time: 6.97 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    List<String> list = new ArrayList<>();
+    
+    public int solution(String word) {
+        int answer = 0;
+        dfs("");
+        answer = list.indexOf(word);
+        
+        return answer;
+    }
+    
+    public void dfs(String str){
+        if(str.length()>5)
+            return;
+        list.add(str);
+        
+        dfs(str+"A");
+        dfs(str+"E");
+        dfs(str+"I");
+        dfs(str+"O");
+        dfs(str+"U");
+    }
+}
