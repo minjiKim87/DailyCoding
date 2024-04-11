fd78af1facba0afe2f336390509ad6593bf4339d
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2024-04-11 13:23:17 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
 class Solution {
+    boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        boolean[] visited = new boolean[computers.length];
+        visited = new boolean[n];
         
-        for(int i = 0;i<computers.length;i++){
+        for(int i =0;i<n;i++){
             if(!visited[i]){
-                dfs(i, computers, visited);
+                visited[i]=true;
+                dfs(computers, i);
                 answer++;
             }
         }
         
-        
         return answer;
     }
     
-    public void dfs(int start, int[][] computers, boolean[] visited){
-        visited[start] = true;
-        
+    public void dfs(int[][] computers, int node){
         for(int i =0;i<computers.length;i++){
-            if(computers[start][i]==1&& !visited[i]){
-                dfs(i, computers, visited);
+            if(!visited[i]&&computers[node][i]==1){
+                 visited[i]=true;
+                dfs(computers, i);
             }
         }
-        
     }
 }
