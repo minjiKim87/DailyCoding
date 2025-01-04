e62bf9327c5903079ae1c040e161a5042475a0e1
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2025-01-04 15:14:54 +0900

    [level 2] Title: 올바른 괄호, Time: 35.92 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,45 @@
 import java.util.*;
 
+
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        boolean answer = true;
         
-        for(char c:s.toCharArray()){
-            if(c=='('){
-                stack.push(c);
-            }else if(stack.isEmpty()){
-                return false;
-            }else{
-                stack.pop();
-            }
+        Stack<Character> origin = new Stack<>();
+        for(int i =s.length()-1;i>=0;i--){
+            origin.add(s.charAt(i));
         }
         
-        boolean answer = stack.isEmpty();
+        if(origin.peek()==')')
+            answer = false;
+        else{
+            
+            Stack<Character> open = new Stack<>();
+            
+            open.push(origin.pop());
+            
+            while(!origin.isEmpty()){
+                 
+                Character c = origin.pop();
+                
+                if(open.isEmpty() && c==')'){
+                    answer = false;
+                    break;
+                }else if(!open.isEmpty() && c==')'){
+                    open.pop();
+                }else if(c =='('){
+                    open.add(c);
+                }
+            }
+            
+            if(open.size()>0)
+                answer = false;
+            
+            
+            
+        }
+        
+
         return answer;
     }
 }
